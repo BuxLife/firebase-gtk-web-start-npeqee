a1dce3617b54a101322761424daa26b8b418b46b
BuxLife <Naeembux@live.com>
2022-09-18 18:05:38 +0200

    Synchronize Messages

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
 import { getAuth, EmailAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { getFirestore, addDoc, collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 
 import * as firebaseui from 'firebaseui';
 
@@ -102,6 +102,20 @@ async function main() {
     input.value = '';
     // Return false to avoid redirect
     return false; 
+  });
+
+  // Create query for messages
+  const q = query(collection(db, 'guestbook'), orderBy('timestamp', 'desc'));
+  onSnapshot(q, snaps => {
+    // Reset page
+    guestbook.innerHTML = '';
+    // Loop through documents in db
+    snaps.forEach(doc => {
+      // Create an HTML entry for each document and add it to the chat. 
+      const entry = document.createElement('p');
+      entry.textContent = doc.data().name + ':' + doc.data().text;
+      guestbook.appendChild(entry);
+    });
   });
 
 }
