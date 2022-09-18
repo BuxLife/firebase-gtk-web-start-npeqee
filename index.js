12e3de68aade75a908756e5c359701aac919c03d
BuxLife <Naeembux@live.com>
2022-09-18 17:29:12 +0200

    Add user sign-in (RSVP)

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import './style.css';
 import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
-import {} from 'firebase/auth';
+import { getAuth, EmailAuthProvider} from 'firebase/auth';
 import {} from 'firebase/firestore';
 
 import * as firebaseui from 'firebaseui';
+
+
 
 // Document elements
 const startRsvpButton = document.getElementById('startRsvp');
@@ -26,11 +28,20 @@ let guestbookListener = null;
 let db, auth;
 
 async function main() {
-  // Add Firebase project configuration object here
-  const firebaseConfig = {};
+  // Web App's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyALfRAz3176FDpGVgRse7rP-n780ygyzJ0",
+    authDomain: "fir-web-codelab-cff6e.firebaseapp.com",
+    projectId: "fir-web-codelab-cff6e",
+    storageBucket: "fir-web-codelab-cff6e.appspot.com",
+    messagingSenderId: "687879305182",
+    appId: "1:687879305182:web:be322c3f22bd195e020f4f"
+  };
 
-  // initializeApp(firebaseConfig);
-
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
+  auth = getAuth();
+  
   // FirebaseUI config
   const uiConfig = {
     credentialHelper: firebaseui.auth.CredentialHelper.NONE,
@@ -46,7 +57,12 @@ async function main() {
       },
     },
   };
+  // Initialize the FirebaseUI widget using Firebase. 
+  const ui = new firebaseui.auth.AuthUI(auth);
 
-  // const ui = new firebaseui.auth.AuthUI(auth);
+  // Listen to RSVP button clicks
+  startRsvpButton.addEventListener("click", () => {
+    ui.start("#firebaseui-auth-container", uiConfig);
+  });
 }
 main();
